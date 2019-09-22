97d53f45388738187095cbc8c5749761e2d0afd2
runoob <test@runoob.com>
2019-09-22 13:20:45 +0800

    init

---
+++ src/main/java/com/zcp/community/CommunityApplication.java
@@ -0,0 +1,15 @@
+package com.zcp.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class CommunityApplication {
+
+    public static void
+    cmdmain(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
